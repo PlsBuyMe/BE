e9860f8b48279204cefe1376962c709e2e0a2410
nam <yhnham@gmail.com>
2024-07-31 17:07:30 +0900

    add file

---
+++ src/main/java/com/example/PlsBuyMe/PlsBuyMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlsBuyMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlsBuyMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlsBuyMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlsBuyMe/PlsBuyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlsBuyMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlsBuyMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
